4a66f0ebc75f2f0ca64aa26aa052dbdd6caab906
Richard Richards <rdrrichards@hotmail.com>
2019-12-13 16:57:52 -0600

    Initial commit

---
+++ ProtocolHandlerConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ProtocolHandlerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Protocol Handler Console!");
+            Console.WriteLine(string.Empty);
+
+            Console.WriteLine($"Full command line: {Environment.CommandLine}");
+            Console.WriteLine(string.Empty);
+
+            Console.WriteLine("Full command line args:");
+
+            args.ToList().ForEach(arg => Console.WriteLine(arg));
+
+            Console.ReadLine();
+        }
+    }
+}
